aaf0589664646a06d932c2210a0c7efdd7076165
David Carreras <dcarreras@diggics.com>
2022-02-09 22:44:26 +0100

    Cambios alumnoB

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -4,6 +4,7 @@ public static void main (String [] args){
     System.out.println("¡Hola Mundo!");
     System.out.println("Hola soy el Alumno B y estoy modificando el archivo");
     System.out.println("Hola soy el Alumno A y estoy modificando el archivo");
+    System.out.println("El Joaquim no es molt llest");
 
 }
 }
