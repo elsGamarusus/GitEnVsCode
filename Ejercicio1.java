36ba4137903d98d5823de09799622f0d2c02423d
linkjoa <79259445+linkjoa@users.noreply.github.com>
2022-02-09 22:08:45 +0100

    Cambios parte2

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -3,7 +3,6 @@ public class Ejercicio1
 public static void main (String [] args){
     System.out.println("¡Hola Mundo!");
     System.out.println("Hola soy el Alumno B y estoy modificando el archivo");
-    
-
+    System.out.println("Hola soy el Alumno A y estoy modificando el archivo");
 }
 }
